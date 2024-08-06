1f1bc39a11b743096839c50747ff7de1ea45cb35
yclown <yclown@163.com>
2024-08-06 17:15:06 +0800

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -158,7 +158,7 @@ namespace MiniWeread
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "微信阅读摸鱼小助手";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
